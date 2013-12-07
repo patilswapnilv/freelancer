aa0849e6e030ce3b89d8fa0bee927ddb25f2d9a9
Namaless <namaless@gmail.com>
2013-12-07 04:30:09 +0100

    Fixed some inconsistence
    Fixed some inconsistence with head comment for plugin informations.

--- freelancer.php
+++ freelancer.php
@@ -8,8 +8,21 @@
  * @package    	Freelancer
  * @since      	0.0.1
  * @author     	Pulido Pereira Nuno Ricardo <pereira@nunoapps.com>
+ * @copyright   Copyright (c) 2007 - 2013, Pulido Pereira Nuno Ricardo
  * @link       	http://nunoapps.com/plugins/freelancer
- * @license   	GPL-2.0+
+ * @license   	http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright 2007 - 2013  Pereira Pulido Nuno Ricardo  (email : pereira@nunoapps.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume 
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @wordpress-plugin
  * Plugin Name:       	Freelancer
@@ -22,25 +35,8 @@
  * License:      		GPL-2.0+
  * License URI:       	http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       	/languages
- * GitHub Plugin URI: 	https://github.com/namaless/freelancer
+ * GitHub Plugin URI: 	https://github.com/nunoapps/freelancer
  */
-
-/*
-	Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : pereira@nunoapps.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 /**
  * Freelancer Main Class.
